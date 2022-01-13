bc85f83610b19235ddbbf3995a988683ebe0b97a
Lionel Morgan <lionel.morgan@revature.net>
2022-01-13 15:17:46 -0800

    create post test

--- src/test/java/stepdefinitions/ForgotPasswordSDF.java
+++ src/test/java/stepdefinitions/ForgotPasswordSDF.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
-import poms.LoginPOM;
+import poms.ForgotPasswordPOM;
 
 public class ForgotPasswordSDF {
     ForgotPasswordPOM forgotPasswordPOM;
